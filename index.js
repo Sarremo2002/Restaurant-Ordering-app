06cc476ccf4f88336d95435e630c6a5ea50c8c20
Selim Mohamed <selimwaleed2@icloud.com>
2025-12-02 14:40:00 +0200

    menu items rendered out

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 import { menuArray } from "./data.js";
 
-console.log(menuArray);
+function getMenuList() {
+  let menu = ``;
+  menuArray.forEach(function (food) {
+    menu += `<div class="item">
+  
+  <div class="emoji">${food.emoji}</div>
+
+  <div class="info">
+    <h2>${food.name}</h2>
+    <p class="desc">${food.ingredients.join(",")}</p>
+    <p class="price">${food.price}</p>
+  </div>
+
+  <button class="add-btn" data-id="${food.id}">+</button>
+
+</div>
+
+<hr />`;
+  });
+  return menu;
+}
+function render() {
+  document.getElementById("menu").innerHTML = getMenuList();
+}
+render();
